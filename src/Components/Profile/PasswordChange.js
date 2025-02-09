d477eb63c7cb1f0813178fc5bdf56e0e932bc3ac
hainweb <hainofficialweb@gmail.com>
2025-02-09 10:39:41 +0530

    Update PasswordChange.js

--- src/Components/Profile/PasswordChange.js
+++ src/Components/Profile/PasswordChange.js
@@ -223,21 +223,21 @@ const PasswordChange = ({ user, setView, isForgot }) => {
                                 {showPasswords.confirm ? <EyeOffIcon className="w-4 h-4" /> : <EyeIcon className="w-4 h-4" />}
                             </button>
                         </div>
-                    </div>
-
+                    </div>     
+                {loading ?
                     <button
                         type="submit"
                         disabled={timeLeft > 0 || passwordStrength.score < 2}
-                        className="w-full flex py-2 px-4 bg-blue-500 hover:bg-blue-600 disabled:bg-blue-300 text-white rounded-lg transition-colors duration-200 disabled:cursor-not-allowed"
-                    >
-                        {loading ?
-                            <div className="flex space-x-1">
+                        className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 disabled:bg-blue-300 text-white rounded-lg transition-colors duration-200 disabled:cursor-not-allowed"
+                    > Change Password</button>
+                        :
+                            <div className="flex space-x-1 bg-blue-500">
                  <Loader2 className="w-4 h-4 animate-spin dark:text-white" />
               </div>
-                            :
-                            'Change Password'
+                            
+                            
                         }
-                    </button>
+                    
                 </form>
             </div>
 
