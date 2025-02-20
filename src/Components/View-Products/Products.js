692050cf0266d2431b335f885b027ca1f7ef7f11
hainweb <hainofficialweb@gmail.com>
2025-02-20 16:35:30 +0530

    Update Products.js

--- src/Components/View-Products/Products.js
+++ src/Components/View-Products/Products.js
@@ -374,7 +374,7 @@ const [showFullDescription, setShowFullDescription] = useState(false);
       <select
         value={selectedOption}
         onChange={(e) => setSelectedOption(e.target.value)}
-        className="mt-1 block w-full rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:border-blue-400"
+        className="mt-1 block w-full text-gray-700 dark:text-gray-300 rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:border-blue-400"
       >
         {product.CustomOptions[0].values.map((val, index) => (
           <option
